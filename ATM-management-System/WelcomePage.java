31a31e441e7fac68ecd94da423b528908f32fc37
Ung-Ouchy <156298357+Ung-Ouchy@users.noreply.github.com>
2024-04-04 21:52:21 +0700

    o

--- ATM-management-System/WelcomePage.java
+++ ATM-management-System/WelcomePage.java
@@ -1,30 +1,29 @@
 import java.util.Scanner;
 public class WelcomePage {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String reply;
-		String replys;
-		CustomerAuthentication obj = new CustomerAuthentication();
-		do
-		{
-			System.out.println("WELCOME TO ATM MANAGEMENT SYSTEM");
-			System.out.println("1. SIGN UP ");
-			System.out.println("2. SIGN IN ");
-			System.out.println("Please select the option");
-			if(sc.hasNextInt()) {
-				int option = sc.nextInt();
-				switch(option)
-				{
+	Scanner sc = new Scanner(System.in);
+	String reply;
+	String replys;
+	CustomerAuthentication obj = new CustomerAuthentication();
+	do
+	{
+	System.out.println("WELCOME TO ATM MANAGEMENT SYSTEM");
+	System.out.println("1. SIGN UP ");
+	System.out.println("2. SIGN IN ");
+	System.out.println("Please select the option");
+		if(sc.hasNextInt()) {
+			int option = sc.nextInt();
+			switch(option)
+			{
 				case 1: obj.signup();
-						obj.writeArrayListToFile();
-						break;
-				case 2: 
-					String x= obj.signin();
-				    if (x=="Y") {
-				    	System.out.println("Invalid username or PIN. Please try again.");
-				    }
-				    else {
-				    	System.out.println("Login successful!");
+					obj.writeArrayListToFile();
+					break;
+				case 2: String x= obj.signin();
+				    	if (x=="Y") {
+				    		System.out.println("Invalid username or PIN. Please try again.");
+				    	}
+				    	else {
+				    		System.out.println("Login successful!");
 				    	do {
 				    		System.out.println("1. Withdraw Money ");
 				    		System.out.println("2. Deposit Money ");
@@ -36,33 +35,33 @@ public class WelcomePage {
 				
 				    		switch(choice)
 				    		{
-								case 1: obj.readArrayListFromFile();
-									obj.withdraw();	
-									obj.writeArrayListToFile();
-									break;							
-								case 2:  obj.readArrayListFromFile();
-									obj.deposit();
-									obj.writeArrayListToFile();
-									break;
-								case 5: 
-									obj.viewaccount();
-									break;							
+							case 1: obj.readArrayListFromFile();
+								obj.withdraw();	
+								obj.writeArrayListToFile();
+								break;							
+							case 2:  obj.readArrayListFromFile();
+								obj.deposit();
+								obj.writeArrayListToFile();
+								break;
+							case 5: obj.viewaccount();
+								break;							
 				    		}
-				System.out.println("Continue? Y/N ");
-	            replys = sc.next();
-		        } while (replys.equalsIgnoreCase("y"));
-				break;
-				}
+		System.out.println("Continue? Y/N ");
+	        replys = sc.next();
+		} 
+		while (replys.equalsIgnoreCase("y"));
+		break;
+		}
 				
-				default:
-				System.out.println("PLEASE ENTER VALID INPUT ");
-			      }
+		default:
+			System.out.println("PLEASE ENTER VALID INPUT ");
+		}
            }
-			else {
+		else {
                 System.out.println("Invalid input. Please enter a number.");
                 sc.next();
             }
-			System.out.println("DO YOU WANT TO CONTINUE Y/N ");
+		System.out.println("DO YOU WANT TO CONTINUE Y/N ");
             reply = sc.next();
         } while (reply.equalsIgnoreCase("y"));
         sc.close(); // Close the scanner when done
